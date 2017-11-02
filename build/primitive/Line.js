94c4fdaee6c0d8fa8048da7548b707e55b5a2e19
Bryce St. Pierre <brycelstpierre@gmail.com>
2017-11-02 00:20:31 -0400

    Updated Line class.

--- build/primitive/Line.js
+++ build/primitive/Line.js
@@ -4,51 +4,63 @@ import Point from './Point';
 export default class Line 
 {
     constructor (...p) {
-        try {
-            // Object: { x : n } or { y : n }.
-            if (p.length === 1 && Type.isObject(p[0])) {
-                // Horizontal.
-                if (p[0].hasOwnProperty('x')) {
-                    this._slope = Infinity;
-                    this._xInt = p[0].x;
-                } // Vertical.
-                else if (p[0].hasOwnProperty('y')) {
-                    this._slope = 0;
-                    this._yInt = p[0].y;
-                }
-            } // Number, Number: slope, yInt.
-            else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
-                this._slope = p[0];
-                this._yInt = p[1];
-            } // Number, Object: slope, Point.
-            else if (p.length === 2 && Type.isNumber(p[0]) && Type.isObject(p[1])) {
-                this._slope = p[0];
-                this._yInt = p[1].y - (this._slope * p[1].x);
-            } // Object, Object: Point, Point.
-            else if (p.length === 2 && Type.isObject(p[1]) && Type.isObject(p[1])) {
+        // Object: { x : n } | { y : n }.
+        if (p.length === 1 && Type.isObject(p[0])) {
+            // Horizontal.
+            if (p[0].hasOwnProperty('x')) {
+                this._xInt = p[0].x;
+            } // Vertical.
+            else if (p[0].hasOwnProperty('y')) {
+                this._slope = 0;
+                this._yInt = p[0].y;
+            } 
+            else if (p[0].hasOwnProperty('slope') && p[0].hasOwnProperty('yInt')) {
+                this._slope = p[0].slope;
+                this._yInt = p[0].yInt;
+            }
+        } // Number, Number: slope, yInt.
+        else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
+            this._slope = p[0];
+            this._yInt = p[1];
+        } // Number, Object: slope, Point.
+        else if (p.length === 2 && Type.isNumber(p[0]) && Type.isObject(p[1])) {
+            this._slope = p[0];
+            this._yInt = p[1].y - (this._slope * p[1].x);
+        } // Object, Object: Point, Point.
+        else if (p.length === 2 && Type.isObject(p[1]) && Type.isObject(p[1])) {
+            if (p[0].x === p[1].x) {
+                this._xInt = p[0].x;
+            } else {
                 this._slope = (p[1].y - p[0].y) / (p[1].x - p[0].x);
                 this._yInt = p[0].y - (this._slope * p[0].x);
             }
-            else
-                throw new Error('LineConstructFailure');
-        } catch (e) {
-            console.error(e.stack);
         }
     }
 
     solveX (y) {
-
+        if (this._xInt)
+            return this._xInt;
+        else if (this._slope !== 0)
+            return (y - this._yInt) / this._slope;
     }
-
+    
     solveY (x) {
-
+        if (this._xInt)
+            return null;
+        else if (this._slope === 0)
+            return this._yInt;
+        else
+            return (this._slope * x) + this._yInt;
     }
 
     atX (x) {
-
+        if (this._xInt)
+            return null;
+        else
+            return new Point(x, this.solveY(x));
     }
 
     atY (y) {
-
+        return new Point(this.solveX(y), y);
     }
 }
--- build/primitive/Point.js
+++ build/primitive/Point.js
@@ -3,74 +3,43 @@ import Type from './Type';
 export default class Point
 {
     constructor (...p) {
-        try {
-            // Default.
-            if (p.length === 0) {
-                this._x = 0;
-                this._y = 0;
-            } // Array: [x, y].
-            else if (p.length === 1 && Type.isArray(p[0])) {
-                this._x = p[0][0];
-                this._y = p[0][1];
-            } // Object: { x: n, y: n }.
-            else if (p.length === 1 && Type.isObject(p[0])) {
-                this._x = p[0].x;
-                this._y = p[0].y;
-            } // Numbers: a, b.
-            else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
-                this._x = p[0];
-                this._y = p[1];
-            } // Default.
-            else 
-                throw new Error('PointConstructError');
-        } catch (e) {
-            console.error(e.stack);
+        // Default.
+        if (p.length === 0) {
+            this._x = 0;
+            this._y = 0;
+        } // Object: {x, y}.
+        else if (p.length === 1 && Type.isObject(p[0])) {
+            this._x = p[0].x;
+            this._y = p[0].y;
+        } // Number, Number: a, b.
+        else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
+            this._x = p[0];
+            this._y = p[1];
         }
     }
 
+    // Object: Point.
+    // Number, Number: x, y.
     add (...p) {
-        try {
-            if (p.length === 1 && Type.isObject(p[0])) {
-                this._x += p[0].x;
-                this._y += p[0].y;
-            } else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
-                this._x += p[0];
-                this._y += p[1];
-            } else
-                throw new Error('PointAddError');
-        } catch (e) {
-            console.error(e.stack);
+        if (p.length === 1 && Type.isObject(p[0])) {
+            this._x += p[0].x;
+            this._y += p[0].y;
+        } else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
+            this._x += p[0];
+            this._y += p[1];
         }
     }
 
+    // Number: n.
+    // Number, Number: n1, n2.
     multiply (...p) {
-        try {
-            if (p.length === 1 && Type.isNumber(p[0])) {
-                this._x *= p[0];
-                this._y *= p[0];
-            } else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
-                this._x *= p[0];
-                this._y *= p[1];
-            } else 
-                throw new Error('PointMultiplyError');
-        } catch (e) {
-            console.error(e.stack);
-        }
-    }
-
-    // rotate () { }
-
-    isNear (point, threshold) {
-        try {
-            if (Type.isObject(point) && Type.isNumber(threshold)) {
-                return Math.sqrt(
-                    ((this._x - point.x) ** 2) + ((this._x - point.y) ** 2)
-                ) < threshold;
-            } else
-                throw new Error('PointIsNearError');
-        } catch (e) {
-            console.error(e.stack);
-        }
+        if (p.length === 1 && Type.isNumber(p[0])) {
+            this._x *= p[0];
+            this._y *= p[0];
+        } else if (p.length === 2 && Type.isNumber(p[0]) && Type.isNumber(p[1])) {
+            this._x *= p[0];
+            this._y *= p[1];
+        } 
     }
     
     get json () {
